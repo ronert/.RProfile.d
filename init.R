1834f61a1c73285a0016b8b267ea6410a3ea4f2a
Ronert Obst <ronert.obst@gmail.com>
2012-08-10 11:06:58 +0100

    modified packages

--- init.R
+++ init.R
@@ -5,44 +5,36 @@ local({
   options(repos = r)
 })
 
-## Check and install packages
-library(utils)
-list.of.packages <- c("devtools", "ggplot2", "reshape", "reshape2", "lubridate", "plyr",
-                      "stringr",  "digest", "mutatr", "brew",
-                      "testthat", "sinartra", "helpr",  "RCurl",
-                      "Rcpp", "XML", "MASS", "RTextTools", "tikzDevice",
-                      "sqldf", "e1071", "foreign", "RMySQL", "RSQLite",
-                      "xtable", "randomForest", "RColorBrewer",
-                      "RODBC", "lattice", "Hmisc", "Sim.DiffProc",
-                      "lme4", "data.table", "PerformanceAnalytics",
-                      "boot", "MCMCglmm", "rpart", "survey",
-                      "twitteR", "Matrix", "rgl", "zoo", "foreach",
-                      "quantmod", "xts", "plm", "multicomp", "glmnet",
-                      "tm", "coin", "caret", "plotrix", "reporttools",
-                      "animation", "xlsReadWrite", "xlsx", "Cairo",
-                      "iterators", "unknownR", "DPpackage",
-                      "fitdistrplus", "IBrokers", "mlbench",
-                      "heatmap.plus", "FNN", "vars", "memoise",
-                      "MHadaptive", "RgoogleMaps", "klaR", "tree",
-                      "rggobi", "classifly", "clusterfly", "meifly",
-                      "profr", "ProjectTemplate", "makeR",
-                      "benchmark", "ElemStatLearn", "mclust",
-                      "flexmix", "magic", "lars", "nnet", "maxLik",
-                      "TeachingDemos", "googleVis", "rainbow",
-                      "orderbook", "segue", "party", "hints",
-                      "mvtnorm", "formatR", "mboost", "ggmap",
-                      "MCMCpack", "ada", "Boruta", "bst", "caTools",
-                      "class", "Cubist", "earth", "elasticnet",
-                      "ellipse", "evtree", "fastICA", "foba", "gam",
-                      "GAMens", "gbm", "glmnet", "grid", "hda",
-                      "HDclassif", "ipred", "kernlab", "klaR",
-                      "kohonen", "KRLS", "lars", "leaps",
-                      "LogicForest", "logicFS", "LogicReg", "mboost", "mda", "mgcv", "mlbench", "neuralnet", "nnet", "nodeHarvest", "obliqueRF", "pamr", "partDSA", "party",
-                      "penalized", "penalizedLDA", "pls", "pROC", "proxy", "qrnn", "quantregForest", "randomForest", "RANN", "rda", "relaxo",
-                      "rFerns", "rocc", "rpart", "rrcov", "RRF", "rrlda", "RSNNS", "RWeka",
-                      "sda", "SDDA", "sparseLDA", "spls", "stepPlr", "superpc", "vbmp")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+## Check and install packages library(utils) list.of.packages <-
+c("devtools", "ggplot2", "reshape", "reshape2", "lubridate", "plyr",
+"stringr", "digest", "mutatr", "brew", "testthat", "sinartra",
+"helpr", "RCurl", "Rcpp", "XML", "MASS", "RTextTools", "tikzDevice",
+"sqldf", "e1071", "foreign", "RMySQL", "RSQLite", "xtable",
+"randomForest", "RColorBrewer", "RODBC", "lattice", "Hmisc",
+"Sim.DiffProc", "lme4", "data.table", "PerformanceAnalytics", "boot",
+"bootstrap", "MCMCglmm", "rpart", "survey", "twitteR", "Matrix",
+"rgl", "zoo", "foreach", "quantmod", "xts", "plm", "multicomp", "tm",
+"coin", "caret", "plotrix", "reporttools", "animation",
+"xlsReadWrite", "xlsx", "Cairo", "iterators", "unknownR", "DPpackage",
+"fitdistrplus", "IBrokers", "mlbench", "heatmap.plus", "FNN", "vars",
+"memoise", "MHadaptive", "RgoogleMaps", "klaR", "tree", "rggobi",
+"classifly", "clusterfly", "meifly", "profr", "ProjectTemplate",
+"makeR", "benchmark", "ElemStatLearn", "mclust", "flexmix", "magic",
+"nnet", "maxLik", "TeachingDemos", "googleVis", "rainbow",
+"orderbook", "segue", "party", "hints", "mvtnorm", "formatR",
+"mboost", "ggmap", "MCMCpack", "ada", "Boruta", "bst", "caTools",
+"class", "Cubist", "earth", "elasticnet", "ellipse", "evtree",
+"fastICA", "foba", "gam", "GAMens", "gbm", "glmnet", "grid", "hda",
+"HDclassif", "ipred", "kernlab", "klaR", "kohonen", "KRLS", "lars",
+"leaps", "LogicForest", "logicFS", "LogicReg", "mboost", "mda",
+"mgcv", "mlbench", "neuralnet", "nnet", "nodeHarvest", "obliqueRF",
+"pamr", "partDSA", "party", "penalized", "penalizedLDA", "pls",
+"pROC", "proxy", "qrnn", "quantregForest", "randomForest", "RANN",
+"rda", "relaxo", "rFerns", "rocc", "rpart", "rrcov", "RRF", "rrlda",
+"RSNNS", "RWeka", "sda", "SDDA", "sparseLDA", "spls", "stepPlr",
+"superpc", "vbmp") new.packages <- list.of.packages[!(list.of.packages
+%in% installed.packages()[,"Package"])] if(length(new.packages))
+install.packages(new.packages)
 
 ## Default Packages
 # old <- getOption("defaultPackages")
